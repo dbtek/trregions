91994a3d2d2f252c724c94fb5f2f30d1c59d34fd
İsmail Demirbilek <ce.demirbilek@gmail.com>
2020-07-06 18:14:02 +0300

    fix turkish search

--- regions/views.py
+++ regions/views.py
@@ -6,17 +6,43 @@ from regions.serializers import CitySerializer, DistrictSerializer, Neighborhood
 from rest_framework import filters
 
 
+class SearchTrFilter(filters.SearchFilter):
+
+    def get_search_terms(self, request):
+        """
+        Search terms are set by a ?search=... query parameter,
+        and may be comma and/or whitespace delimited.
+        """
+        params = request.query_params.get(self.search_param, '')
+        params = params.replace('\x00', '')  # strip null characters
+        params = params.replace(',', ' ')
+
+        tr_map = {
+            'i': 'İ',
+            'ç': 'Ç',
+            'ğ': 'Ğ',
+            'ö': 'Ö',
+            'ş': 'Ş',
+            'ü': 'Ü'
+        }
+
+        for char in tr_map:
+            params = params.replace(char, tr_map[char])
+
+        return params.split()
+
+
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [SearchTrFilter]
     search_fields = ('title',)
 
 
 class DistrictViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filter_backends = [SearchTrFilter, DjangoFilterBackend]
     filter_fields = ('city',)
     search_fields = ('title',)
 
@@ -24,6 +50,6 @@ class DistrictViewSet(viewsets.ReadOnlyModelViewSet):
 class NeighborhoodsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Neighborhood.objects.all()
     serializer_class = NeighborhoodSerializer
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filter_backends = [SearchTrFilter, DjangoFilterBackend]
     filter_fields = ('district', 'district__city')
     search_fields = ('title',)
